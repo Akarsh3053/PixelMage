6f2b107176501c8e5f2b3f044d033d63260b3e45
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-03-18 21:20:52 +0530

    add cloudinary

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -286,14 +286,14 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             disabled={isTransforming || newTransformation === null}
             onClick={onTransformHandler}
           >
-            {isTransforming ? 'Transforming...' : 'Apply Transformation'}
+            {isTransforming ? 'Casting a spell...' : 'Apply Transformation'}
           </Button>
           <Button 
             type="submit"
             className="submit-button capitalize"
             disabled={isSubmitting}
           >
-            {isSubmitting ? 'Submitting...' : 'Save Image'}
+            {isSubmitting ? 'Brewing potion...' : 'Save Image'}
           </Button>
         </div>
       </form>
